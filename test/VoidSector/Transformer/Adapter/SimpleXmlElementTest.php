fac19c0ee8250010eaa059239bcf93ff803c514b
void-sector <noinmea@gmail.com>
2014-10-14 23:01:55 +0200

    Rename

---
+++ test/VoidSector/Transformer/Adapter/SimpleXmlElementTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace test\VoidSector\Transformer\Adapter;
+
+use PHPUnit_Framework_TestCase;
+
+class SimpleXmlElementtTest extends PHPUnit_Framework_TestCase
+{
+    public function testloadFromString()
+    {
+        $this->assertEquals(
+            new \SimpleXmlElement('<foo><bar /></foo>'),
+            $this->getHandler()->loadFromString('<foo><bar /></foo>')
+        );
+    }
+    
+    
+    public function testloadFromFileOrUrl()
+    {
+        $xmlFile = __DIR__ . '/../../../xmlDataProvider/1.book/structure.xml';
+        
+        $this->assertEquals(
+            new \SimpleXmlElement($xmlFile, 0, true),
+            $this->getHandler()->loadFromFileOrUrl($xmlFile)
+        );
+    }
+
+    
+    /**
+     * Helper Function
+     * @return \SimXml\Transformer\Adapter\SimpleXmlElement
+     */
+    private function getHandler()
+    {
+        return new \VoidSector\Transformer\Adapter\SimpleXmlElement();
+    }    
+}
