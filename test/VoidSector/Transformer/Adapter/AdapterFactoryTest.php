7bf0dffe07abdbf1545f79afe4e7c6955446745b
void-sector <noinmea@gmail.com>
2014-10-14 23:13:14 +0200

    hmm

--- test/VoidSector/Transformer/Adapter/AdapterFactoryTest.php
+++ test/VoidSector/Transformer/Adapter/AdapterFactoryTest.php
@@ -1,36 +1,36 @@
 <?php
 
-namespace test\VoidSector\Transformer\Adapter;
-
-use InvalidArgumentException;
-use PHPUnit_Framework_TestCase;
-use VoidSector\Transformer\Adapter\AdapterFactory;
-
-
-class AdapterFactoryTest extends PHPUnit_Framework_TestCase
-{
-    public function testGetDefaultTransformAdapter()
-    {
-        $this->assertInstanceOf(
-            'VoidSector\Transformer\Adapter\SimpleXmlElement',
-            AdapterFactory::getAdapter('SimpleXmlElement')
-        );
-    }
-
-    
-    public function testGetDomDocumentTransformAdapter()
-    {
-        $this->assertInstanceOf(
-            'VoidSector\Transformer\Adapter\DOMDocument',
-            AdapterFactory::getAdapter('DOMDocument')
-        );
-    }
-
-    /**
-     * @expectedException InvalidArgumentException
-     */
-    public function testNoneExistingTransformAdapter()
-    {
-        AdapterFactory::getAdapter('fooBar');
-    }
-}
+//namespace test\VoidSector\Transformer\Adapter;
+//
+//use InvalidArgumentException;
+//use PHPUnit_Framework_TestCase;
+//use VoidSector\Transformer\Adapter\AdapterFactory;
+//
+//
+//class AdapterFactoryTest extends PHPUnit_Framework_TestCase
+//{
+//    public function testGetDefaultTransformAdapter()
+//    {
+//        $this->assertInstanceOf(
+//            'VoidSector\Transformer\Adapter\SimpleXmlElement',
+//            AdapterFactory::getAdapter('SimpleXmlElement')
+//        );
+//    }
+//
+//    
+//    public function testGetDomDocumentTransformAdapter()
+//    {
+//        $this->assertInstanceOf(
+//            'VoidSector\Transformer\Adapter\DOMDocument',
+//            AdapterFactory::getAdapter('DOMDocument')
+//        );
+//    }
+//
+//    /**
+//     * @expectedException InvalidArgumentException
+//     */
+//    public function testNoneExistingTransformAdapter()
+//    {
+//        AdapterFactory::getAdapter('fooBar');
+//    }
+//}
